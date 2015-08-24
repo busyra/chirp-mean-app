bbff838cc9e39d4e9ae7850774bd88f4ec6ddd47
Busyra Anwar <busyra@me.com>
2015-08-23 19:21:26 -0700

    adds feed and post function

--- js/chirpApp.js
+++ js/chirpApp.js
@@ -1,5 +1,15 @@
+
 var app = angular.module('chirpApp',[]);
 
-app.controller =('mainController', function($scope){
+app.controller('mainController', function($scope){
+  console.log("connected")
 
+  $scope.posts = [];
+  $scope.newPost ={ create_by: '', text: '', create_at: ''};
+
+  $scope.post = function(){
+    $scope.newPost.created_at = Date.now();
+    $scope.posts.push($scope.newPost);
+    $scope.newPost = {created_by: '', text:'', created_at:''};
+  };
 });
