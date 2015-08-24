969bf0c009d687223ae87756efbd3fa5f054b9e8
Busyra Anwar <busyra@me.com>
2015-08-24 01:37:48 -0700

    fixes indentation

--- js/chirpApp.js
+++ js/chirpApp.js
@@ -2,14 +2,14 @@
 var app = angular.module('chirpApp',[]);
 
 app.controller('mainController', function($scope){
-  console.log("connected")
+    console.log("connected");
 
-  $scope.posts = [];
-  $scope.newPost ={ create_by: '', text: '', create_at: ''};
+    $scope.posts = [];
+    $scope.newPost ={ create_by: '', text: '', create_at: ''};
 
-  $scope.post = function(){
-    $scope.newPost.created_at = Date.now();
-    $scope.posts.push($scope.newPost);
-    $scope.newPost = {created_by: '', text:'', created_at:''};
-  };
+    $scope.post = function(){
+        $scope.newPost.created_at = Date.now();
+        $scope.posts.push($scope.newPost);
+        $scope.newPost = {created_by: '', text:'', created_at:''};
+    };
 });
