870a1cea9ffa339b13372fb1d0900d7c4bb7ab45
Busyra Anwar <busyra@me.com>
2015-08-29 20:50:58 -0700

    adds Users schema

---
+++ models/models.js
@@ -0,0 +1,7 @@
+var mongoose = require('mongoose');
+
+var userSchema = new mongoose.Schema({
+    username: String,
+    password: String, //hash created from password
+    created_at: {type: Date, default: Date.now}
+});
