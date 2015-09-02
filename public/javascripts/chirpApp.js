d720f0cbf4cbc32fc6b6a8342d8f257c6e1cce49
Busyra Anwar <busyra@me.com>
2015-09-02 00:02:30 -0700

    adds signout and updates signup and log in function in chirpApp

--- public/javascripts/chirpApp.js
+++ public/javascripts/chirpApp.js
@@ -1,5 +1,14 @@
 
-var app = angular.module('chirpApp',['ngRoute']);
+var app = angular.module('chirpApp',['ngRoute']).run(function($rootScope){
+    $rootScope.authenticated = false;
+    $rootScope.current_user = '';
+
+    $rootScope.signout = function(){
+        $http.get('auth/signout');
+        $rootScope.authenticated = false;
+        $rootScope.current_user = '';
+    };
+});
 
 app.config(function($routeProvider){
   $routeProvider
@@ -19,33 +28,48 @@ app.config(function($routeProvider){
       controller: 'authController'
     });
 });
-
-app.controller('mainController', function($scope){
-    console.log("connected");
-
-    $scope.posts = [];
-    $scope.newPost ={ create_by: '', text: '', create_at: ''};
-
-    $scope.post = function(){
-        $scope.newPost.created_at = Date.now();
-        $scope.posts.push($scope.newPost);
-        $scope.newPost = {created_by: '', text:'', created_at:''};
-    };
+app.factory('postService', function($resource){
+	return $resource('/api/posts/:id');
 });
 
-app.controller('authController', function($scope){
+app.controller('mainController', function($scope){
+    $scope.posts = postService.query();
+    	$scope.newPost = {created_by: '', text: '', created_at: ''};
+
+    	$scope.post = function() {
+    	  $scope.newPost.created_by = $rootScope.current_user;
+    	  $scope.newPost.created_at = Date.now();
+    	  postService.save($scope.newPost, function(){
+    	    $scope.posts = postService.query();
+    	    $scope.newPost = {created_by: '', text: '', created_at: ''};
+    	  });
+    	};
+});
+
+app.controller('authController', function($scope, $rootScope, $http, $location){
     $scope.user = {username: '', password: ''};
     $scope.error_message= '';
 
-    // postService.getAll().success(function(data){
-    //     $scope.posts = data;
-    // });
     $scope.login = function(){
-        //placeholder until auth is implemented
-        $scope.error_message = 'login request for ' + $scope.user.username;
+        $http.post('/auth/login',$scope.user).success(function(data){
+            if(data.state == 'success'){
+                $rootScope.authenticated = true;
+                $rootScope.current_user = data.user.username;
+                $location.path('/');
+            }else{
+                $scope.error_message = data.message;
+            }
+        });
     };
     $scope.register = function (){
-        //placeholder until auth is implemented
-        $scope.error_message = 'registration request for' + $scope.user.username;
+        $http.post('/auth/signup',$scope.user).success(function(data){
+            if(data.state == 'success'){
+                $rootScope.authenticated = true;
+                $rootScope.current_user = data.user.username;
+                $location.path('/');
+            }else{
+                $scope.error_message = data.message;
+            }
+        });
     };
 });
